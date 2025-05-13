7f029cf134b1173c10acf84e23810fb19f5258bf
Smit Patel <95951014+smit1304@users.noreply.github.com>
2025-05-13 11:25:26 -0400

    Fixed api key leak

--- backend/test.js
+++ backend/test.js
@@ -2,7 +2,7 @@ import { GoogleGenAI } from "@google/genai";
 import dotenv from "dotenv";                        // Import dotenv to read environment variables from .env file
 
 dotenv.config();                                    // Load all the .env variables into process.env
-const ai = new GoogleGenAI({ apiKey: "AIzaSyA1Jip0qqDvxQm1NnTHBC33kzjybEw5f_k" });
+const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY }); // Create a new instance of GoogleGenAI with the API key from environment variables
 console.log("Gemini API Key:", process.env.GOOGLE_API_KEY); // debug this
 
 async function main() {
